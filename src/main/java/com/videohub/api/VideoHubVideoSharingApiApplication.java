530df6f08663656afa0f0bddba44431a2b681625
bedo <bodymosad67@gmail.com>
2024-10-26 14:34:53 +0300

    🎉 init project

---
+++ src/main/java/com/videohub/api/VideoHubVideoSharingApiApplication.java
@@ -0,0 +1,13 @@
+package com.videohub.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoHubVideoSharingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoHubVideoSharingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/videohub/api/VideoHubVideoSharingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videohub.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoHubVideoSharingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
